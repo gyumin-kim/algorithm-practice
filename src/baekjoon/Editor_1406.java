7abf69544e9229a12f185b2f13561bd7ea88fce1
Gyumin Kim <starkying@gmail.com>
2018-10-31 01:36:34 +0900

    Editor_1406: 구현 완료(백준 시간초과)

--- src/baekjoon/Editor_1406.java
+++ src/baekjoon/Editor_1406.java
@@ -1,20 +1,28 @@
 package baekjoon;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.io.OutputStreamWriter;
 
+/**
+ * https://www.acmicpc.net/problem/1406
+ * IDE에서 3가지 예제 입력에 대해 올바른 결과가 출력됨.
+ * 백준 제출 결과 시간초과됨.
+ */
 public class Editor_1406 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out), 512);
         String input = br.readLine();
+        StringBuilder sb = new StringBuilder(input);
         int time = Integer.parseInt(br.readLine());
         int pointer = input.length();
-        LinkedList<String> list = new LinkedList<>();
 
         for (int i = 0; i < time; i++) {
             String command = br.readLine();
-            switch (command.charAt(0)) {
+            char opt = command.charAt(0);
+            switch (opt) {
                 // 커서를 왼쪽으로 한칸 옮김 (커서가 문장의 맨 앞이면 무시됨)
                 case 'L':
                     if (pointer != 0)
@@ -22,34 +30,24 @@ public class Editor_1406 {
                     break;
                 // 커서를 오른쪽으로 한 칸 옮김 (커서가 문장의 맨 뒤이면 무시됨)
                 case 'D':
-                    if (pointer < list.size())
+                    if (pointer < sb.length())
                         ++pointer;
                     break;
                 // 커서 왼쪽에 있는 문자를 삭제함 (커서가 문장의 맨 앞이면 무시됨)
                 // 삭제로 인해 커서는 한 칸 왼쪽으로 이동한 것처럼 나타나지만, 실제로 커서의 오른쪽에 있던 문자는 그대로임
                 case 'B':
-                    if (pointer != 0) {
-
-                    }
+                    if (pointer != 0)
+                        sb.delete(pointer-1, pointer--);
                     break;
                 // 'extraStr'을 커서 왼쪽에 추가함
                 case 'P':
-                    String extraStr = command.split(" ")[1];
-                    list.add(pointer++, extraStr);
+                    sb.insert(pointer++, command.charAt(2));
                     break;
             }
         }
 
-        for (String str : list)
-            //TODO: StringBuilder 혹은 StringBuffer로 출력할 것(성능 향상)
-            System.out.print(str);
-
-//        LinkedList<String> list = new LinkedList<>();
-//        list.addLast("a");
-//        list.addLast("b");
-//        list.addLast("c");
-//        list.addLast("d");
-//        list.add(4, "e");
-//        System.out.println(list);
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
     }
 }
