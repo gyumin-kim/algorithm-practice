3c036723d874575f2ae10570db714e45bbf8d3c0
Gyumin Kim <starkying@gmail.com>
2018-10-30 18:41:41 +0900

    Josephus_1158: ArrayList -> LinkedList 변경. 주석 추가.

--- src/baekjoon/Josephus_1158.java
+++ src/baekjoon/Josephus_1158.java
@@ -2,11 +2,16 @@ package baekjoon;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * https://www.acmicpc.net/problem/1158
+ * 백준 제출 결과는 성공.
+ * 풀고 보니 LinkedList의 특성을 전혀 활용하지 않는 풀이인 것 같아 개선이 필요한 것 같다.
+ */
 public class Josephus_1158 {
-    public static void printAnswer(List<Integer> list, int m, int n) {
+    static void printAnswer(List<Integer> list, int m, int n) {
         boolean[] isRemoved = new boolean[m];
 
         System.out.print("<");
@@ -18,7 +23,8 @@ public class Josephus_1158 {
                 if (index >= m) {
                     index = 0;
                     if (isRemoved[index]) {
-                        while (index < m && isRemoved[index])    index++;
+                        while (index < m && isRemoved[index])
+                            index++;
                     }
                 }
             }
@@ -42,7 +48,7 @@ public class Josephus_1158 {
         String[] input = bf.readLine().split(" ");
         int m = Integer.parseInt(input[0]);
         int n = Integer.parseInt(input[1]);
-        List<Integer> list = new ArrayList<>();
+        List<Integer> list = new LinkedList<>();
 
         // 1부터 m까지의 정수를 list에 저장
         for (int i = 1; i <= m; i++)
