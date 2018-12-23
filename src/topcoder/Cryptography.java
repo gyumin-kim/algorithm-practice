f0590d28998b663e2609a2e076dcef529315ffbe
Gyumin Kim <starkying@gmail.com>
2018-12-23 10:56:46 +0900

    Add comments for each method

--- src/topcoder/Cryptography.java
+++ src/topcoder/Cryptography.java
@@ -4,6 +4,9 @@ import java.util.Arrays;
 
 public class Cryptography {
 
+  /**
+   * 이중 for문을 사용하여 일일이 구한다(전체 탐색).
+   */
   private static long encrypt(int[] numbers) {
     int length = numbers.length;
     long answer = 0;
@@ -24,6 +27,10 @@ public class Cryptography {
     return answer;
   }
 
+  /**
+   * numbers에서 원래 수들을 모두 곱한 것과, 임의의 하나의 수를 +1하고 모든 수를 곱한 것의 증가율은 (n+1) / n이다. 따라서 n이 작을수록 곱한 값이
+   * 커지므로, numbers에서 가장 작은 수를 +1한 뒤 모든 수를 곱한 것이 가장 큰 값이 된다.
+   */
   private static long encrypt2(int[] numbers) {
     Arrays.sort(numbers);
     numbers[0] += 1;
