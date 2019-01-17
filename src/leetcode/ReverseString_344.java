357ace88cf57def8759fc78f2ea2c198b661ecd5
Gyumin Kim <starkying@gmail.com>
2019-01-17 20:28:54 +0900

    Success

---
+++ src/leetcode/ReverseString_344.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+public class ReverseString_344 {
+
+  public String reverseString(String s) {
+    StringBuilder sb = new StringBuilder();
+    int length = s.length();
+    for (int i = length - 1; i >= 0; i--) {
+      sb.append(s.charAt(i));
+    }
+
+    return sb.toString();
+  }
+}
