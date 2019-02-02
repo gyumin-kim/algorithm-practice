95d9b727c815c0caed2a861dbabc24acff051d88
Gyumin Kim <starkying@gmail.com>
2019-02-02 18:21:40 +0900

    recursive한 방식 추가. README 완성

--- src/leetcode/Q206_Reverse_Linked_List/README.md
+++ src/leetcode/Q206_Reverse_Linked_List/README.md
@@ -19,3 +19,16 @@ A linked list can be reversed either iteratively or recursively. Could you imple
 My Idea & Solution
 -------------
 
+1. 반복하는 방식은 하나의 loop 안에서 총 3개의 링크를 수정한다는 것이 포인트다. 최초에 맨 앞에 있었던 노드가
+맨 뒤까지, 즉 next가 null이 될 때까지 반복한다.  
+*time complexity*: `O(n)`  
+*space complexity*: `O(1)`  
+
+2. 재귀적인 방식은 Solution을 참고하였다. 노드의 위치를 움직인다는 느낌이 아니라, 모든 링크의 방향을 반대로 
+바꾼다는 느낌으로 생각하면 이해하는 데 도움이 된다.  
+원래 노드들이 자신의 오른쪽 노드를 next로 갖고 있었다면, 맨 마지막 노드부터 하나씩 그 방향을 왼쪽으로 바꿔주는 것이다. 
+자신의 오른쪽에 있던 노드의 next를 자기 자신으로 바꾸어 화살표 방향을 전환한다.  
+재귀 흐름의 가장 안쪽(head.next가 null인 것)에서 자기 자신(head)을 리턴하면, 그 바깥 재귀함수에서는 그것을 `p`로 
+받는다. 그 p는 모든 재귀함수 호출이 끝났을 때 최종적으로 리턴되는 레퍼런스가 되는데, 그로 인해 리스트 전체를 reverse시킨 결과가 된다.  
+*time complexity*: `O(n)`  
+*space complexity*: `O(n)` (재귀호출로 인해 stack에 메소드가 쌓인다. n은 리스트의 길이) 
--- src/leetcode/Q206_Reverse_Linked_List/Solution.java
+++ src/leetcode/Q206_Reverse_Linked_List/Solution.java
@@ -19,10 +19,17 @@ public class Solution {
 
     ListNode pointer = head;
     while (pointer.next != null) {
-      ListNode toFront = pointer.next;
+      ListNode pointerNextNode = pointer.next;
+
+      /*
+        여기서 총 3개의 링크를 바꿔준다. 이 작업을 pointer가 맨 마지막 노드가 될 때까지 반복한다.
+        1. pointer(현재 가리키고 있는 곳)가 가리키는 링크 -> 다음다음 노드를 가리키도록
+        2. front(pointer 다음에 있는 노드)가 가리키는 링크 -> 현재 head가 가리키고 있는 곳(맨 앞 노드)을 가리키도록
+        3. head -> 방금 전에 맨 앞으로 이동한 노드(front)를 가리키도록
+       */
       pointer.next = pointer.next.next;
-      toFront.next = head;
-      head = toFront;
+      pointerNextNode.next = head;
+      head = pointerNextNode;
     }
     return head;
   }
@@ -31,6 +38,14 @@ public class Solution {
    * recursive한 방식
    */
   public ListNode reverseListRecursive(ListNode head) {
-    return null;
+    if (head == null || head.next == null)
+      return head;
+
+    ListNode p = reverseListRecursive(head.next);
+    head.next.next = head;
+    head.next = null;
+
+    // 모든 노드가 뒤집힌 후, p가 첫번째 노드가 된다.
+    return p;
   }
 }
